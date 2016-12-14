75d271b86748e41e2a498654150ab530b3c2c00d
Steve Mao <maochenyan@gmail.com>
2016-12-14 21:57:01 +1100

    refactor the chartInfo logic to container component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -6,57 +6,15 @@ class PageLink extends React.Component {
   render() {
     return (
       <div>
-        {this.props.data.quizzes.map((quiz) => {
-          let correct = 0;
-          let incorrect = 0;
-          let unfinished = 0;
-          const answers = this.props.answers[quiz.id];
+        {this.props.chartInfo.map((info) => (
+          <div key={info.id}>
+            <h3>
+              {info.title}
+            </h3>
+            <PieChart data={info.chartData} />
+          </div>
+        ))}
 
-          this.props.data.questions.forEach((question) => {
-            let answer;
-            if (answers) {
-              answer = answers[question.id];
-            } else {
-              answer = null;
-            }
-
-            if (quiz.question_ids.indexOf(question.id) !== -1) {
-              if (answer === question.correct_answer) {
-                correct++;
-              } else if (answer != null) {
-                incorrect++;
-              } else {
-                unfinished++;
-              }
-            }
-          });
-
-          const chartData = [{
-            color: '#46BFBD',
-            highlight: '#5AD3D1',
-            label: 'Correct',
-            value: correct
-          }, {
-            color: '#F7464A',
-            highlight: '#FF5A5E',
-            label: 'Incorrect',
-            value: incorrect
-          }, {
-            color: '#949FB1',
-            highlight: '#A8B3C5',
-            label: 'Unfinished',
-            value: unfinished
-          }];
-
-          return (
-            <div key={quiz.id}>
-              <h3>
-                {quiz.title}
-              </h3>
-              <PieChart data={chartData} />
-            </div>
-          );
-        })}
         <Link to='/1'>
           Start over
         </Link>
--- src/containers/Progress.js
+++ src/containers/Progress.js
@@ -2,9 +2,60 @@ import { connect } from 'react-redux';
 import Progress from '../components/Progress';
 
 export function mapStateToProps(state) {
+  const ret = [];
+  const data = state.questions.data;
+
+  data.quizzes.forEach((quiz) => {
+    let correct = 0;
+    let incorrect = 0;
+    let unfinished = 0;
+    const answers = state.answers[quiz.id];
+
+    data.questions.forEach((question) => {
+      let answer;
+      if (answers) {
+        answer = answers[question.id];
+      } else {
+        answer = null;
+      }
+
+      if (quiz.question_ids.indexOf(question.id) !== -1) {
+        if (answer === question.correct_answer) {
+          correct++;
+        } else if (answer != null) {
+          incorrect++;
+        } else {
+          unfinished++;
+        }
+      }
+    });
+
+    const chartData = [{
+      color: '#46BFBD',
+      highlight: '#5AD3D1',
+      label: 'Correct',
+      value: correct
+    }, {
+      color: '#F7464A',
+      highlight: '#FF5A5E',
+      label: 'Incorrect',
+      value: incorrect
+    }, {
+      color: '#949FB1',
+      highlight: '#A8B3C5',
+      label: 'Unfinished',
+      value: unfinished
+    }];
+
+    ret.push({
+      id: quiz.id,
+      title: quiz.title,
+      chartData
+    });
+  });
+
   return {
-    data: state.questions.data,
-    answers: state.answers
+    chartInfo: ret
   };
 }
 
